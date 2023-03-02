8fb644d7f86e6baebfd1dc81f162b101da00aa66
curranod <curranod840@gmail.com>
2023-03-02 11:34:42 -0600

    wk4d4 assignment

---
+++ week4/day4/assignment/assignment.js
@@ -0,0 +1,23 @@
+const newsUl = document.getElementById('newsUl')
+
+async function getIds() {
+    const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+    const array = await response.json()
+    array.map((id) => getArticles(id))
+}
+
+async function getArticles(articleId) {
+    const id = articleId
+    const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+    const article = await response.json()
+    const date = new Date(article.time * 1000)
+
+    const story = `
+    <h1> ${article.title}</h1>
+    <a href="${article.url}">Article link</a>
+    <li>By: ${article.by}</li>
+    <li>date: ${date.toLocaleDateString('en-US')}</li>
+    `
+    newsUl.innerHTML += story
+}
+getIds()
